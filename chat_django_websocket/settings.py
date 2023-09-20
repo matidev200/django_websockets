0c6a7bde0572f4ff3d158c49cce524e32f6f2434
Matias <matiasdv1@hotmail.com>
2023-09-20 18:12:12 -0300

    fix(REDIS): Redis database removed

--- chat_django_websocket/settings.py
+++ chat_django_websocket/settings.py
@@ -42,8 +42,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173/",
-    "http://localhost:5174/",
+    "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 ROOT_URLCONF = 'chat_django_websocket.urls'
